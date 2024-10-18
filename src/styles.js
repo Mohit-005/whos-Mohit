b049bf6f8f2238cdf5f09cc249de5a9fc38d17b3
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-10-18 18:22:59 +0530

    add styles

--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,11 @@
+/*
+   Copyright (C), 2023-2024, Mohit Kumar (Mohit)
+   Author: Mohit Kumar
+   FileName: ContactForm.jsx
+   Version: I
+   Creation: 02/08/2024
+   Last modification: 03/06/2023
+*/
 const styles = {
   paddingX: 'sm:px-16 px-6',
   paddingY: 'sm:py-16 py-6',
