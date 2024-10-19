0de4c968e87591117a6421de04fca9bbd8c616a3
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-10-19 12:52:24 +0530

    Update constants.js

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -147,29 +147,8 @@ const projects = [
     description: 'Automated Leetcode Solver: Built a bot to fetch, solve and submit Leetcode problems programatically',
     image: 'https://i.ibb.co/jHtstmw/Leet-Code-Auto-Solver.png',
     source_code_link: 'https://github.com/Mohit-005/Leetcode-Plus/tree/main5',
-    demo_link: 'https://github.com/Mohit-005/Leetcode-Plus/blob/main/README.md',
+    demo_link: 'https://mohit-005.github.io/Leetcode-Plus/',
   },
-  // {
-  //   name: 'Project 7',
-  //   description: 'This project implements the bullshit I have been working on',
-  //   image: "https://camo.githubusercontent.com/5d85ae990f2b7b9a0f4d6246168b7995fc930d59d127a15c3c4ea18eb2a5001b/68747470733a2f2f6c756d696572652d612e616b616d616968642e6e65742f76312f696d616765732f65735f686f7573652d6f662d6469736e65792d706c75735f6d6f625f6d5f35376437663836652e6a7065673f726567696f6e3d302c302c3830302c3630302677696474683d373638",
-  //   source_code_link: 'https://github.com/',
-  //   // demo_link: 'https://disneyplusreplica.netlify.app',  ------->>>>> Demo Link
-  // },
-  // {
-  //   name: 'Project 8',
-  //   description: 'This project implements the bullshit I have been working on',
-  //   image: "https://camo.githubusercontent.com/94f800b79d759c06717af7d816a16ccff75a0101c942ca9ad3c562d0dd2c9404/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f76312e59326c6b505463354d4749334e6a45784f4870736432527a5a575a6a616e4e366547513364473172616e64755a576b306457357262336f79596d527964584636646a413264535a6c634431324d563970626e526c636d35686246396e61575a66596e6c666157516d593351395a772f634b4c57543837305a4a39587769706e57772f67697068792e676966",
-  //   source_code_link: 'https://github.com/',
-  //   // demo_link: 'https://camo.githubusercontent.com/94f800b79d759c06717af7d816a16ccff75a0101c942ca9ad3c562d0dd2c9404/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f76312e59326c6b505463354d4749334e6a45784f4870736432527a5a575a6a616e4e366547513364473172616e64755a576b306457357262336f79596d527964584636646a413264535a6c634431324d563970626e526c636d35686246396e61575a66596e6c666157516d593351395a772f634b4c57543837305a4a39587769706e57772f67697068792e676966',  ------->>>>> Demo Link
-  // },
-  // {
-  //   name: 'Project 9',
-  //   description: 'This project implements the bullshit I have been working on',
-  //   image: "https://i.pinimg.com/originals/96/c3/9a/96c39a5cb89092760fe11f355a32f4a4.gif",
-  //   source_code_link: 'https://github.com/',
-  //   // demo_link: 'https://camo.githubusercontent.com/59b3c9969f369559fbeb1ed6a53f159e0e83b1151b82552b5b2165c4e9686e89/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f312f31612f4669626f6e616363695f63616c6c5f747265655f352e676966', ------->>>>> Demo Link
-  // }
 ];
 
 const memoji = {
